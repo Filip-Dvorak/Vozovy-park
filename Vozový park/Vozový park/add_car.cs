0e8f601a08db98fc97af2a2275ab6a94142b8999
Filip-Dvorak <82036520+Filip-Dvorak@users.noreply.github.com>
2021-04-12 17:22:05 +0200

    Add files via upload

---
+++ Vozový park/Vozový park/add_car.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vozový_park
+{
+    public partial class add_car : Form
+    {
+        public add_car()
+        {
+            InitializeComponent();
+
+            foreach(KeyValuePair<int,Car> c in Car.cars)
+            {
+                listBox1.Items.Add(c.Key + "; " + c.Value.znacka + "; " + c.Value.model + "; " + c.Value.typ + "; " + c.Value.spotreba + "l/100km" + ";");
+            }
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            if(textBoxID.Text =="" || textBoxZnacka.Text == "" || textBoxModel.Text == "" || textBoxSpotreba.Text=="")
+            {
+                MessageBox.Show("Vyplňte všechny údaje");
+                return;
+            }
+            Car car = new Car();
+            if(int.TryParse(textBoxID.Text, out car.id) == false)
+            {
+                MessageBox.Show("Špatná hodnota id (očekává se číslo)");
+            }
+            
+            car.znacka = textBoxZnacka.Text;
+            car.model = textBoxModel.Text;
+            try
+            {
+                car.typ = comboBox1.SelectedItem.ToString();
+            }
+            catch(NullReferenceException)
+            {
+                MessageBox.Show("Vybrte typ vozu");
+                return;
+            }
+            car.isActive = true;
+
+            if(double.TryParse(textBoxSpotreba.Text, out car.spotreba)){
+                car.spotreba = double.Parse(textBoxSpotreba.Text);
+            }
+            else
+            {
+                MessageBox.Show("Špatná hodnota spotřeby");
+                return;
+            }
+
+            foreach (var entry in Car.cars)
+            {
+                if (int.Parse(textBoxID.Text) == entry.Key)
+                {
+                    MessageBox.Show("Id je již zabrané");
+                    return;
+                }
+
+            }
+
+            Car._cars.Add(int.Parse(textBoxID.Text), car);
+            listBox1.Items.Add(car.id + "; " + car.znacka + "; " + car.model + "; " + car.typ + "; " + car.spotreba + "l/100km" + ";");
+        }
+
+        private void odstavit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] car = listBox1.SelectedItem.ToString().Split(';');
+                int id = int.Parse(car[0]);
+
+
+                foreach (KeyValuePair<int, Reservation> r in Reservation.reservations.ToList())
+                {
+                    if (r.Value.idAuto == Car.cars[id].id)
+                    {
+                        if (DateTime.Compare(r.Value.from, DateTime.Now) > 0)
+                        {
+                            Reservation._reservations.Remove(r.Key);
+                        }
+                    }
+                }
+
+
+
+                Car._cars[id].isActive = false;
+                MessageBox.Show("Vůz byl odstaven");
+            }
+            catch(NullReferenceException)
+            {
+                MessageBox.Show("Vyberte vůz");
+            }
+        }
+
+        private void aktivovat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] car = listBox1.SelectedItem.ToString().Split(';');
+                int id = int.Parse(car[0]);
+
+                Car._cars[id].isActive = true;
+                MessageBox.Show("Vůz byl přidán do provozu");
+            }
+            catch(NullReferenceException)
+            {
+                MessageBox.Show("Vyberte vůz");
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] car = listBox1.SelectedItem.ToString().Split(';');
+                int id = int.Parse(car[0]);
+
+
+                foreach (KeyValuePair<int, Reservation> r in Reservation.reservations.ToList())
+                {
+                    if (r.Value.idAuto == Car.cars[id].id)
+                    {
+                        Reservation._reservations.Remove(r.Key);
+                    }
+                }
+
+                foreach (KeyValuePair<int, NakladyNaUdrzbu> n in NakladyNaUdrzbu.naklady.ToList())
+                {
+                    if (n.Value.idAuta == Car.cars[id].id)
+                    {
+                        NakladyNaUdrzbu._naklady.Remove(n.Key);
+                    }
+
+                }
+
+
+
+                Car._cars.Remove(id);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                MessageBox.Show("Vůz byl odebrán");
+            }
+            catch(NullReferenceException)
+            {
+                MessageBox.Show("Vyberte vůz");
+            }
+        }
+
+        private void Return_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Vozový park/Vozový park/admin_cancel_res.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vozový_park
+{
+    public partial class admin_cancel_res : Form
+    {
+        public admin_cancel_res()
+        {
+            InitializeComponent();
+
+            try
+            {
+                foreach (KeyValuePair<int, Reservation> r in Reservation.reservations)
+                {
+                    if (DateTime.Compare(r.Value.to, DateTime.Now) > 0 || DateTime.Compare(r.Value.to, DateTime.Now) == 0)
+                    {
+                        listBox1.Items.Add(r.Value.idReservation + ", " + Car.cars[r.Value.idAuto].znacka + ", " + User.users[r.Value.idUser].jmeno + ", " + User.users[r.Value.idUser].prijmeni + ", " + r.Value.from.ToShortDateString() + " - " + r.Value.to.ToShortDateString());
+                    }
+                }
+            }
+            catch(KeyNotFoundException)
+            {
+
+            }
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string s = listBox1.SelectedItem.ToString();
+                string[] rezervace = s.Split(',');
+                int idRezervace = int.Parse(rezervace[0]);
+
+                Reservation._reservations.Remove(idRezervace);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                MessageBox.Show("Rezervace byla zrušena");
+            }
+            catch(NullReferenceException)
+            {
+                MessageBox.Show("Vyberte rezervaci ke zrušení");
+                return;
+            }
+        }
+
+        private void Return_OnClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
